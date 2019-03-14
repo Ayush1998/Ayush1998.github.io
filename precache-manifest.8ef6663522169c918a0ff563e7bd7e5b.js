f65b78bd9e5c686573d823affaeaf55e5a39e560
Ayush1998 <ayushmudgal11@gmail.com>
2019-03-14 16:59:39 +0530

    Updates

---
+++ precache-manifest.8ef6663522169c918a0ff563e7bd7e5b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a697fcc4ed0577334cda3f9df06f43e5",
+    "url": "/static/media/ring2.a697fcc4.gif"
+  },
+  {
+    "revision": "a1d40c3876bd1460ff1e9e3858d699c7",
+    "url": "/static/media/profile.a1d40c38.png"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0da050ae52c160471765",
+    "url": "/static/js/main.6f082f8c.chunk.js"
+  },
+  {
+    "revision": "b549983a3d7b30ddc15a",
+    "url": "/static/js/2.c55d5580.chunk.js"
+  },
+  {
+    "revision": "0da050ae52c160471765",
+    "url": "/static/css/main.2bb1b37f.chunk.css"
+  },
+  {
+    "revision": "4dce5a76eabb84e550b9c90ccd541226",
+    "url": "/index.html"
+  }
+];
